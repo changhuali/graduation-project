bc8be04bc65619d9f0fb9433bba39a293d3b61ed
changhua.li <changhua.li@socialcredits.cn>
2016-03-22 18:35:56 +0800

    导航位置调整

--- src/app/js/components/common/Header.js
+++ src/app/js/components/common/Header.js
@@ -9,18 +9,8 @@ export default class Header extends Component {
         }
     }
 
-    componentDidMount() {
-
-    }
-
     logout() {
         this.props.userBoundAc.logout();
-    }
-
-    showUserSet() {
-        this.setState({
-            showUserSet: !this.state.showUserSet,
-        })
     }
 
     render() {
@@ -33,19 +23,7 @@ export default class Header extends Component {
                         <span className="header-left-tog">[切换城市]</span>
                     </div>
                     {this.props.user.info.id ?
-                        <ul className="header-right">
-                            <li>咨询热线:400-400-888</li>
-                            <li>关注微信</li>
-                            <li onClick={this.showUserSet.bind(this)} className="header-user">
-                                <a href="javascript:;"><i className="fa fa-user"></i>&nbsp;&nbsp;{this.props.user.info.userName}</a>
-                                {this.state.showUserSet ?
-                                <ul className="header-user-ul">
-                                    <li><a href="/">个人设置</a></li>
-                                    <li><a onClick={this.logout.bind(this)} href="javascript:;">退出</a></li>
-                                </ul>
-                                : ""}
-                            </li>
-                        </ul>
+                        <UserSetting {...this.props} logout={this.logout.bind(this)} />
                     :
                         <ul className="header-right">
                             <li><Link style={{color: "#999999"}} to={{pathname: "login"}}>请登录</Link></li>
@@ -56,6 +34,51 @@ export default class Header extends Component {
                     }
                 </div>
             </div>
+        )
+    }
+}
+
+class UserSetting extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            showUserSet: false,
+        }
+    }
+
+    showUserSet() {
+        this.setState({
+            showUserSet: !this.state.showUserSet,
+        })
+    }
+
+    logout() {
+        this.props.logout();
+    }
+
+    componentDidMount() {
+        var node = document.getElementById("header_user");
+        var width = node.clientWidth;
+        this.setState({
+            margin: width/2,
+        })
+    }
+
+    render(){
+        return (
+            <ul className="header-right">
+                <li>咨询热线:400-400-888</li>
+                <li>关注微信</li>
+                <li id="header_user" onClick={this.showUserSet.bind(this)} className="header-user">
+                    <a href="javascript:;"><i className="fa fa-user"></i>&nbsp;&nbsp;{this.props.user.info.userName}</a>
+                    {this.state.showUserSet ?
+                    <ul style={{marginLeft: this.state.margin}} className="header-user-ul">
+                        <li><a href="/">个人设置</a></li>
+                        <li><a onClick={this.logout.bind(this)} href="javascript:;">退出</a></li>
+                    </ul>
+                    : ""}
+                </li>
+            </ul>
         )
     }
 }
