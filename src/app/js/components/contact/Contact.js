7d6cc02ca325616c82e5ef6bb17749294fa57b39
changhua.li <changhua.li@socialcredits.cn>
2016-04-11 16:15:07 +0800

    联系我们页面完成

--- src/app/js/components/contact/Contact.js
+++ src/app/js/components/contact/Contact.js
@@ -10,7 +10,17 @@ export default class Contact extends Component {
 
     render() {
         return (
-            <div></div>
+            <div className="contact-wrap">
+              <div className="contact-head"></div>
+              <div className="contact clearfix">
+                <div className="contact-map"></div>
+                <div className="contact-info">
+                  <p className="contact-item"><i className="fa fa-phone"></i><span>电话: 11111111</span></p>
+                  <p className="contact-item"><i className="fa fa-envelope-o"></i><span>传真: 22222222</span></p>
+                  <p className="contact-item"><i className="fa fa-map-marker"></i><span>地址: 重庆市沙坪坝大学城重庆科技学院</span></p>
+                </div>
+              </div>
+            </div>
         )
     }
 }
