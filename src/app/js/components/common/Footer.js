b98235e28a96f2aecb96a48202b07f4c6573ae01
changhua.li <changhua.li@socialcredits.cn>
2016-04-21 18:20:28 +0800

    Footer土巴兔->国风

--- src/app/js/components/common/Footer.js
+++ src/app/js/components/common/Footer.js
@@ -26,7 +26,7 @@ export default class Footer extends Component {
                         {this.createItem()}
                     </p>
                     <p>免责声明：本网站部分内容由用户自行上传，如权利人发现存在误传其作品情形，请及时与本站联系。</p>
-                    <p>© 2016 Tubatu.com 土巴兔装修网和设计师社区 保留所有权利</p>
+                    <p>© 2016 Tubatu.com 国风装修网和设计师社区 保留所有权利</p>
                     <p>中国装修网 粤ICP备08125558号</p>
                 </div>
             </div>
