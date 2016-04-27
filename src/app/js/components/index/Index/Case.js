83d2f4c455e8e6135b9cc8928cadd446602f0216
changhua.li <changhua.li@socialcredits.cn>
2016-04-27 14:58:40 +0800

    首页家装案例连接到终端页

--- src/app/js/components/index/Index/Case.js
+++ src/app/js/components/index/Index/Case.js
@@ -1,42 +1,70 @@
 import React, { Component } from 'react';
-import case_1 from '../../../../images/case_1.jpg';
-import case_2 from '../../../../images/case_2.jpg';
-import case_3 from '../../../../images/case_3.jpg';
-import case_4 from '../../../../images/case_4.jpg';
+import {routerShape} from 'react-router';
+import Loading from '../../common/Loading';
+import NotFound from '../../common/NotFound';
 
 export default class Case extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: this.props.familyCase.list.data,
+        }
+    }
 
+    getCurrData() {
+        return this.state.data.slice(0, 4);
+    }
+
+    viewDetail(obj) {
+        localStorage.setItem('caseDetail', JSON.stringify(obj));
+        if(obj) {
+            this.context.router.push({pathname: '/familyCase/'+obj._id});
         }
     }
 
     createItem() {
         var list = [];
-        DATA.map((obj, idx) => {
+        var data = this.getCurrData();
+        data.map((obj, idx) => {
             list.push(
-                <div key={obj.text+idx} className="index-case-item">
-                    <img src={obj.img} />
-                    <p>{obj.text}</p>
+                <div key={'case'+idx} onClick={this.viewDetail.bind(this, obj)} className="index-case-item">
+                    <img src={obj.img_5} />
+                    <p>{obj.title}</p>
                 </div>
             )
         })
         return list;
     }
 
+    componentDidMount() {
+        this.props.familyCaseBoundAc.getFamilyCaseList();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.familyCase.list.data != undefined) {
+            this.setState({
+                data: nextProps.familyCase.list.data,
+            })
+        }
+    }
+
     render() {
         return (
             <div className="index-case clearfix">
-                {this.createItem()}
+                {this.state.data == undefined
+                    ?
+                    <Loading />
+                    :
+                    this.state.data.length == 0
+                    ?
+                    <NotFound message="暂时没有相关案列" />
+                    :
+                    this.createItem()
+                }
             </div>
         )
     }
 }
-
-const DATA = [
-    {img: case_1, text: '现代风'},
-    {img: case_2, text: '混搭风'},
-    {img: case_3, text: '北欧风'},
-    {img: case_4, text: '简欧风'},
-];
+Case.contextTypes = {
+    router: routerShape.isRequired,
+}
